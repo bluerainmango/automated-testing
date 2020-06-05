bcdc27b6f6faa1cc20410ade42ed781db9f11bd3
Emily Yu <bluerainmango@gmail.com>
2020-06-05 03:30:38 -0700

    puppeteer: with class

--- puppeteer-tricks/build/tests/example.test.js
+++ puppeteer-tricks/build/tests/example.test.js
@@ -4,15 +4,23 @@ var _puppeteer = require("puppeteer");
 
 var _puppeteer2 = _interopRequireDefault(_puppeteer);
 
+var _homepage = require("./homepage");
+
+var _homepage2 = _interopRequireDefault(_homepage);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
+// const puppeteer = require('puppeteer')
+//! Used import of ES6 syntax that puppeteer cannot recognize for Babel test
 describe("Loads URLs", function () {
   it("should work", async function () {
     var browser = await _puppeteer2.default.launch({ headless: false });
     var page = await browser.newPage();
-    await page.goto("http://zero.webappsecurity.com/");
+
+    // await page.goto("http://zero.webappsecurity.com/");
+    var homepage = new _homepage2.default(page);
+    await homepage.visit();
     await page.waitFor(5000);
     await browser.close();
   });
-}); // const puppeteer = require('puppeteer')
-//! Used import of ES6 syntax that puppeteer cannot recognize for Babel test
+});
--- puppeteer-tricks/tests/example.test.js
+++ puppeteer-tricks/tests/example.test.js
@@ -1,12 +1,16 @@
 // const puppeteer = require('puppeteer')
 //! Used import of ES6 syntax that puppeteer cannot recognize for Babel test
 import puppeteer from "puppeteer";
+import Homepage from "./homepage";
 
 describe("Loads URLs", () => {
   it("should work", async () => {
     const browser = await puppeteer.launch({ headless: false });
     const page = await browser.newPage();
-    await page.goto("http://zero.webappsecurity.com/");
+
+    // await page.goto("http://zero.webappsecurity.com/");
+    const homepage = new Homepage(page);
+    await homepage.visit();
     await page.waitFor(5000);
     await browser.close();
   });
