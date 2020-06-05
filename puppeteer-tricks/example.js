c82d7e4398ed9048d9eb3687c6c2e9aa701d26b0
Emily Yu <bluerainmango@gmail.com>
2020-06-04 20:21:32 -0700

    puppeteer: generating pdf, emulating devices

--- puppeteer-tricks/example.js
+++ puppeteer-tricks/example.js
@@ -1,9 +1,32 @@
 const puppeteer = require("puppeteer");
+// const devices = require("puppeteer/DeviceDescriptors");
 
+// //! Full page screenshot
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto("https://example.com/", { waitUntil: "networkidle0" });
+
+  //* waitUntil: "networkidle0" => wait till the page load is finished
+  await page.goto("https://pptr.dev", { waitUntil: "networkidle0" });
   await page.screenshot({ path: "exmaple.png", fullPage: true });
   await browser.close();
+})();
+
+//! PDF
+(async () => {
+  const browser = await puppeteer.launch({ headless: true });
+  const page = await browser.newPage();
+  await page.goto("https://example.com/", { waitUntil: "networkidle0" });
+  await page.pdf({ path: "example.pdf", format: "Letter" });
+  await browser.close();
+})();
+
+//! Emulating devices
+(async () => {
+  const browser = await puppeteer.launch({ headless: false });
+  const page = await browser.newPage();
+  await page.emulate(puppeteer.devices["iPhone X"]);
+  await page.goto("https://pptr.dev");
+  await page.waitFor(10000);
+  await page.close();
 })();
