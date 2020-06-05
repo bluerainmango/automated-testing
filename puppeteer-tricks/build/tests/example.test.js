ce8ed6637836b98a3851710c101edf2da230c092
Emily Yu <bluerainmango@gmail.com>
2020-06-05 03:48:50 -0700

    puppeteer: with mocha-steps

--- puppeteer-tricks/build/tests/example.test.js
+++ puppeteer-tricks/build/tests/example.test.js
@@ -8,19 +8,38 @@ var _homepage = require("./homepage");
 
 var _homepage2 = _interopRequireDefault(_homepage);
 
+var _mochaSteps = require("mocha-steps");
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-// const puppeteer = require('puppeteer')
-//! Used import of ES6 syntax that puppeteer cannot recognize for Babel test
-describe("Loads URLs", function () {
-  it("should work", async function () {
-    var browser = await _puppeteer2.default.launch({ headless: false });
-    var page = await browser.newPage();
+describe("Tests", function () {
+  var browser = void 0;
+  var page = void 0;
 
+  before(async function () {
+    browser = await _puppeteer2.default.launch({ headless: false });
+    page = await browser.newPage();
+    page.setDefaultTimeout(7000);
+  });
+
+  after(async function () {
+    await browser.close();
+  });
+
+  (0, _mochaSteps.step)("step 1: should load homepage", async function () {
     // await page.goto("http://zero.webappsecurity.com/");
     var homepage = new _homepage2.default(page);
     await homepage.visit();
-    await page.waitFor(5000);
-    await browser.close();
   });
-});
+
+  (0, _mochaSteps.step)("step2", async function () {
+    await page.waitForSelector("#fail");
+  });
+  (0, _mochaSteps.step)("step3", async function () {
+    await page.waitForSelector("#fail");
+  });
+  (0, _mochaSteps.step)("step4", async function () {
+    await page.waitForSelector("#fail");
+  });
+}); // const puppeteer = require('puppeteer')
+//! Used import of ES6 syntax that puppeteer cannot recognize for Babel test
--- puppeteer-tricks/tests/example.test.js
+++ puppeteer-tricks/tests/example.test.js
@@ -2,16 +2,35 @@
 //! Used import of ES6 syntax that puppeteer cannot recognize for Babel test
 import puppeteer from "puppeteer";
 import Homepage from "./homepage";
+import { step } from "mocha-steps";
 
-describe("Loads URLs", () => {
-  it("should work", async () => {
-    const browser = await puppeteer.launch({ headless: false });
-    const page = await browser.newPage();
+describe("Tests", () => {
+  let browser;
+  let page;
 
+  before(async () => {
+    browser = await puppeteer.launch({ headless: false });
+    page = await browser.newPage();
+    page.setDefaultTimeout(7000);
+  });
+
+  after(async () => {
+    await browser.close();
+  });
+
+  step("step 1: should load homepage", async () => {
     // await page.goto("http://zero.webappsecurity.com/");
     const homepage = new Homepage(page);
     await homepage.visit();
-    await page.waitFor(5000);
-    await browser.close();
+  });
+
+  step("step2", async () => {
+    await page.waitForSelector("#fail");
+  });
+  step("step3", async () => {
+    await page.waitForSelector("#fail");
+  });
+  step("step4", async () => {
+    await page.waitForSelector("#fail");
   });
 });
