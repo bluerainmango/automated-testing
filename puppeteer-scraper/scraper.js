f163151c976b339a28c73fb38dcdaa3f3ea6546c
Emily Yu <bluerainmango@gmail.com>
2020-06-05 07:03:27 -0700

    puppeteer: scraping data from web with random-useragent

--- puppeteer-scraper/scraper.js
+++ puppeteer-scraper/scraper.js
@@ -1,6 +1,7 @@
 //*** Web Scraper for books */
 
 const puppeteer = require("puppeteer")
+const random_useragent = require("random-useragent")
 const { url } = require("./config")
 
 ;(async () => {
@@ -9,6 +10,23 @@ const { url } = require("./config")
 
 	page.setDefaultTimeout(10000)
 	await page.setViewport({ width: 1200, height: 800 })
+
+	//! Set browser user's agent info with random info
+	await page.setUserAgent(random_useragent.getRandom())
+
+	//! Scrape data from page
+	const name_selector = ".product_main > h1"
+	const price_selector = ".price_color"
+	await page.goto(url)
+	await page.waitForSelector(name_selector)
+	await page.waitForSelector(price_selector)
+	const name = await page.$eval(name_selector, (el) => el.textContent)
+	const price = await page.$eval(price_selector, (el) => el.textContent)
+
+	const nameTrim = name.trim()
+	const priceTrim = price.trim()
+
+	console.log(`Name: ${nameTrim}, Price: ${priceTrim}`)
 
 	await browser.close()
 })().catch((err) => {
